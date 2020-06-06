62e5fda4c8d2dc2ab74ab6207ec44f2934e53f99
Roman <zipo.ckorop@gmail.com>
2020-06-06 13:22:35 +0300

    added more tests for TraceableEventDispatcher

--- tests/TraceableEventDispatcherTest.php
+++ tests/TraceableEventDispatcherTest.php
@@ -15,10 +15,23 @@ namespace Zippovich2\EventDispatcher\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Zippovich2\EventDispatcher\EventDispatcher;
-use Zippovich2\EventDispatcher\Tests\Fixtures\BalanceUpdateSubscriber;
+use Zippovich2\EventDispatcher\EventWatcher;
+use Zippovich2\EventDispatcher\Tests\Fixtures\Bug1\SubscriberA;
+use Zippovich2\EventDispatcher\Tests\Fixtures\Bug1\SubscriberB;
+use Zippovich2\EventDispatcher\Tests\Fixtures\Bug1\SubscriberC;
+use Zippovich2\EventDispatcher\Tests\Fixtures\Bug1\SubscriberD;
+use Zippovich2\EventDispatcher\Tests\Fixtures\ComplexNestedEvents\SubscriberA as ComplexCaseSubscriberA;
+use Zippovich2\EventDispatcher\Tests\Fixtures\ComplexNestedEvents\SubscriberB as ComplexCaseSubscriberB;
+use Zippovich2\EventDispatcher\Tests\Fixtures\ComplexNestedEvents\SubscriberC as ComplexCaseSubscriberC;
+use Zippovich2\EventDispatcher\Tests\Fixtures\ComplexNestedEvents\SubscriberD as ComplexCaseSubscriberD;
+use Zippovich2\EventDispatcher\Tests\Fixtures\ComplexNestedEvents\SubscriberE as ComplexCaseSubscriberE;
+use Zippovich2\EventDispatcher\Tests\Fixtures\ComplexNestedEvents\SubscriberF as ComplexCaseSubscriberF;
+use Zippovich2\EventDispatcher\Tests\Fixtures\ComplexNestedEvents\SubscriberG as ComplexCaseSubscriberG;
+use Zippovich2\EventDispatcher\Tests\Fixtures\ComplexNestedEvents\SubscriberH as ComplexCaseSubscriberH;
+use Zippovich2\EventDispatcher\Tests\Fixtures\PekariumCase\BalanceUpdateSubscriber;
+use Zippovich2\EventDispatcher\Tests\Fixtures\PekariumCase\TransactionSubscriber;
 use Zippovich2\EventDispatcher\Tests\Fixtures\Subscriber;
 use Zippovich2\EventDispatcher\Tests\Fixtures\SubscriberWithTraceableEventDispatcher;
-use Zippovich2\EventDispatcher\Tests\Fixtures\TransactionSubscriber;
 use Zippovich2\EventDispatcher\TraceableEventDispatcher;
 
 class TraceableEventDispatcherTest extends TestCase
@@ -30,7 +43,7 @@ class TraceableEventDispatcherTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->dispatcher = new TraceableEventDispatcher();
+        $this->dispatcher = new TraceableEventDispatcher(new EventWatcher());
     }
 
     protected function tearDown(): void
@@ -96,7 +109,7 @@ class TraceableEventDispatcherTest extends TestCase
         $this->dispatcher->subscribe('event1', [$subscriber, 'callback2']);
         $this->dispatcher->subscribe('event2', [$subscriber, 'callback2']);
 
-        $this->dispatcher->dispatch('event1');
+        $callStackTree = $this->dispatcher->dispatch('event1');
 
         $expected = [
             [
@@ -112,7 +125,7 @@ class TraceableEventDispatcherTest extends TestCase
             ],
         ];
 
-        static::assertEquals($expected, $this->dispatcher->getCallStackTree());
+        static::assertEquals($expected, $callStackTree);
         static::assertEquals(1, $subscriber->callback1InvokedTimes);
         static::assertEquals(2, $subscriber->callback2InvokedTimes);
     }
@@ -181,7 +194,7 @@ class TraceableEventDispatcherTest extends TestCase
         $this->dispatcher->subscribe('BonusTransactionCompletedEvent', [$transactionSubscriber, 'creditBalance']);
         $this->dispatcher->subscribe('BalanceUpdatedEvent', [$balanceUpdateSubscriber, 'notifyUser']);
 
-        $this->dispatcher->dispatch('TransactionCompletedEvent');
+        $callStackTree = $this->dispatcher->dispatch('TransactionCompletedEvent');
 
         $expected = [
             [
@@ -210,12 +223,152 @@ class TraceableEventDispatcherTest extends TestCase
             ],
         ];
 
-        static::assertEquals($expected, $this->dispatcher->getCallStackTree());
+        static::assertEquals($expected, $callStackTree);
         static::assertEquals(2, $transactionSubscriber->creditBalanceInvokedTimes);
         static::assertEquals(1, $transactionSubscriber->calculateBonusesInvokedTimes);
         static::assertEquals(1, $transactionSubscriber->sendEmailInvokedTimes);
         static::assertEquals(2, $balanceUpdateSubscriber->notifyUserInvokedTimes);
 
         static::assertTrue(true);
+    }
+
+    public function testBug1()
+    {
+        $subscriberA = new SubscriberA($this->dispatcher);
+        $subscriberB = new SubscriberB($this->dispatcher);
+        $subscriberC = new SubscriberC($this->dispatcher);
+        $subscriberD = new SubscriberD($this->dispatcher);
+
+        $this->dispatcher->subscribe('A', [$subscriberA, 'handle']);
+        $this->dispatcher->subscribe('B', [$subscriberB, 'handle']);
+        $this->dispatcher->subscribe('C', [$subscriberC, 'handle']);
+        $this->dispatcher->subscribe('D', [$subscriberD, 'handle']);
+
+        $callStackTree = $this->dispatcher->dispatch('A');
+
+        $expected = [
+            [
+                'subscriber' => \sprintf('%s::%s', \get_class($subscriberA), 'handle'),
+                'children' => [
+                    [
+                        'subscriber' => \sprintf('%s::%s', \get_class($subscriberB), 'handle'),
+                        'children' => [
+                            [
+                                'subscriber' => \sprintf('%s::%s', \get_class($subscriberD), 'handle'),
+                            ],
+                        ],
+                    ],
+                    [
+                        'subscriber' => \sprintf('%s::%s', \get_class($subscriberC), 'handle'),
+                    ],
+                ],
+            ],
+        ];
+
+        static::assertEquals($expected, $callStackTree);
+    }
+
+    /**
+     * Testing nested tree:
+     *  -A
+     *  --B
+     *  ---C
+     *  --D
+     *  -A
+     *  --E
+     *  ---B
+     *  ----C
+     *  --F
+     *  --G
+     *  --H
+     *  ---E
+     *  ----B
+     *  -----C.
+     */
+    public function testGetCallStackTreeWithComplexNestedEvents(): void
+    {
+        $subscriberA = new ComplexCaseSubscriberA($this->dispatcher);
+        $subscriberB = new ComplexCaseSubscriberB($this->dispatcher);
+        $subscriberC = new ComplexCaseSubscriberC($this->dispatcher);
+        $subscriberD = new ComplexCaseSubscriberD($this->dispatcher);
+        $subscriberE = new ComplexCaseSubscriberE($this->dispatcher);
+        $subscriberF = new ComplexCaseSubscriberF($this->dispatcher);
+        $subscriberG = new ComplexCaseSubscriberG($this->dispatcher);
+        $subscriberH = new ComplexCaseSubscriberH($this->dispatcher);
+
+        $this->dispatcher->subscribe('A', [$subscriberA, 'handle']);
+        $this->dispatcher->subscribe('A', [$subscriberA, 'handle2']);
+        $this->dispatcher->subscribe('B', [$subscriberB, 'handle']);
+        $this->dispatcher->subscribe('C', [$subscriberC, 'handle']);
+        $this->dispatcher->subscribe('D', [$subscriberD, 'handle']);
+        $this->dispatcher->subscribe('E', [$subscriberE, 'handle']);
+        $this->dispatcher->subscribe('F', [$subscriberF, 'handle']);
+        $this->dispatcher->subscribe('G', [$subscriberG, 'handle']);
+        $this->dispatcher->subscribe('H', [$subscriberH, 'handle']);
+
+        $callStackTree = $this->dispatcher->dispatch('A');
+
+        $expected = [
+            [
+                'subscriber' => \sprintf('%s::%s', \get_class($subscriberA), 'handle'),
+                'children' => [
+                    [
+                        'subscriber' => \sprintf('%s::%s', \get_class($subscriberB), 'handle'),
+                        'children' => [
+                            [
+                                'subscriber' => \sprintf('%s::%s', \get_class($subscriberC), 'handle'),
+                            ],
+                        ],
+                    ],
+                    [
+                        'subscriber' => \sprintf('%s::%s', \get_class($subscriberD), 'handle'),
+                    ],
+                ],
+            ],
+            [
+                'subscriber' => \sprintf('%s::%s', \get_class($subscriberA), 'handle2'),
+                'children' => [
+                    [
+                        'subscriber' => \sprintf('%s::%s', \get_class($subscriberE), 'handle'),
+                        'children' => [
+                            [
+                                'subscriber' => \sprintf('%s::%s', \get_class($subscriberB), 'handle'),
+                                'children' => [
+                                    [
+                                        'subscriber' => \sprintf('%s::%s', \get_class($subscriberC), 'handle'),
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    [
+                        'subscriber' => \sprintf('%s::%s', \get_class($subscriberF), 'handle'),
+                    ],
+                    [
+                        'subscriber' => \sprintf('%s::%s', \get_class($subscriberG), 'handle'),
+                    ],
+                    [
+                        'subscriber' => \sprintf('%s::%s', \get_class($subscriberH), 'handle'),
+                        'children' => [
+                            [
+                                'subscriber' => \sprintf('%s::%s', \get_class($subscriberE), 'handle'),
+                                'children' => [
+                                    [
+                                        'subscriber' => \sprintf('%s::%s', \get_class($subscriberB), 'handle'),
+                                        'children' => [
+                                            [
+                                                'subscriber' => \sprintf('%s::%s', \get_class($subscriberC), 'handle'),
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        static::assertEquals($expected, $callStackTree);
     }
 }
